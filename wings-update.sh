f474490f39b93606469559007ca675f48f803b48
Kotto01 <kotslitemail@gmail.com>
2024-03-13 23:11:20 -0500

    Updated wings updater

--- wings-update.sh
+++ wings-update.sh
@@ -3,8 +3,12 @@
 
 # Begin update process for wings binary
 systemctl stop wings
+echo "Stopped wings"
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+echo "New binary downloaded"
 chmod u+x /usr/local/bin/wings
+echo "New binary properties changed"
 
 # Restart wings to apply update
 systemctl restart wings
+echo "Wings restarted"
