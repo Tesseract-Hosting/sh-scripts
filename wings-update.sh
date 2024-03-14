048bc7b81949158dc0b24f7e8b0ea22f48b89d56
Kotto01 <kotslitemail@gmail.com>
2024-03-13 23:35:54 -0500

    Added colors to wings-update.sh

--- wings-update.sh
+++ wings-update.sh
@@ -1,14 +1,19 @@
-# curl https://raw.githubusercontent.com/Kotto01/tesseract-ind/main/wings-update.sh --output wings-update.sh && sh wings-update.sh
+# curl -s -L https://raw.githubusercontent.com/Kotto01/tesseract-ind/main/wings-update.sh | bash
 #!/bin/bash
+
+# Preettyyyyy...
+REDFRG="\e[41m"
+ENDCLR="\e[0m"
+WHTBKG="\e[97m"
 
 # Begin update process for wings binary
 systemctl stop wings
-echo "Stopped wings"
+echo -e "${WHTBKG}${REDFRG}[TI Wings Update]${ENDCLR}   Stopped wings"
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
-echo "New binary downloaded"
+echo -e "${WHTBKG}${REDFRG}[TI Wings Update]${ENDCLR}   New binary downloaded"
 chmod u+x /usr/local/bin/wings
-echo "New binary properties changed"
+echo -e "${WHTBKG}${REDFRG}[TI Wings Update]${ENDCLR}   New binary properties changed"
 
 # Restart wings to apply update
 systemctl restart wings
-echo "Wings restarted"
+echo -e "${WHTBKG}${REDFRG}[TI Wings Update]${ENDCLR}   Wings restarted"
